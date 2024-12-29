b3b119abb58fcb0ccd55a114f99af0ddfc7f8d5b
Anamika <anamika2004abc@gmail.com>
2024-12-29 12:02:16 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -10,13 +10,17 @@ function showpoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionInput = document.querySelector("#user-instructions");
+  let instructionInput = document.querySelector("#user-instruction");
   let apikey = "8714aed335fo8d04bt89a4b5c1266e26";
-  let prompt = "User instruction: Write a poem in english";
+  let prompt = `User instruction: Write a poem in english about ${instructionInput.value}`;
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.Don't write html at the top. Write a different poem everytime.";
 
   let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionInput.value}</div>`;
 
   axios.get(apiurl).then(showpoem);
 }
