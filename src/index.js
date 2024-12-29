79534f48b306835c1ebdfb07c3b4078782975772
Anamika <anamika2004abc@gmail.com>
2024-12-29 11:32:22 +0530

    adding Api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function showpoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instructions");
+  let apikey = "8714aed335fo8d04bt89a4b5c1266e26";
+  let prompt = "User instruction: Write a poem in english";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.Don't write html at the top. Write a different poem everytime.";
+
+  let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+  axios.get(apiurl).then(showpoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
